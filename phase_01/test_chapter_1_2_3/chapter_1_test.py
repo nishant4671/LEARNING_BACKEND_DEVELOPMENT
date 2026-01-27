966f445b4b20df08a65aa52bfa7074e0304b4193
nishant4671 <nishantkr4671@gmail.com>
2026-01-27 23:05:37 +0530

    jan 27 2025

---
+++ phase_01/test_chapter_1_2_3/chapter_1_test.py
@@ -0,0 +1,26 @@
+# You are building a simple inventory system. Define a Python data structure to represent a single item. It must store:
+
+# An item name (string)
+
+# A quantity (integer)
+
+# A price per unit (float)
+
+# A list of supplier IDs (list of strings)
+
+# Write the Python code to create one such item and assign it to a variable named widget. Use sample data of your choice.
+
+
+
+
+
+widget = {
+    "name": "Steel Widget",
+    "quantity": 120,
+    "price_per_unit": 49.99,
+    "supplier_ids": ["SUP001", "SUP014", "SUP021"]
+}
+
+# this is an exmaple 
+
+
