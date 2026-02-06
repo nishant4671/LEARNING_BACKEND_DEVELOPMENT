5221ff0562fcb8aab424deec319e39f4c44ccce4
nishant4671 <nishantkr4671@gmail.com>
2026-02-07 00:34:03 +0530

    feb 7

---
+++ how_to_create_virtual_enviroment.py
@@ -0,0 +1,9 @@
+# 1. Navigate to your project directory
+cd path\to\your\project
+
+# 2. Create virtual environment
+python -m venv venv
+
+# 3. Activate it (PowerShell specific)
+.\venv\Scripts\Activate.ps1
+
